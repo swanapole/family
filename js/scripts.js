1581e2d864517f98a96b0877dcbe72f5fca272c8
ronnieoginga <oginga001ronney@gmail.com>
2018-12-19 17:11:47 +0300

    initialprofile structure

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+
+// profile js
+	$(document).ready(function() {
+
+
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    });
+});
+
+$(function() {
+$('#profile-image1').on('click', function() {
+$('#profile-image-upload').click();
+});
+});
