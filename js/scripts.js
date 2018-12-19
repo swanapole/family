a035f79f50c56671df98da344f99a1ea4cc83d60
ronnieoginga <oginga001ronney@gmail.com>
2018-12-19 17:46:51 +0300

    functioning prof

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@
             var reader = new FileReader();
 
             reader.onload = function (e) {
-                $('.profile-pic').attr('src', e.target.result);
+                $('#profile-image1').attr('src', e.target.result);
             }
 
             reader.readAsDataURL(input.files[0]);
@@ -16,17 +16,17 @@
     }
 
 
-    $(".file-upload").on('change', function(){
+    $("#profile-image-upload").on('change', function(){
         readURL(this);
     });
 
-    $(".upload-button").on('click', function() {
-       $(".file-upload").click();
+   $('#profile-image1').on('click', function() {
+       $("#profile-image-upload").click();
     });
 });
 
-$(function() {
-$('#profile-image1').on('click', function() {
-$('#profile-image-upload').click();
-});
-});
+// $(function() {
+// $('#profile-image1').on('click', function() {
+// $('#profile-image-upload').click();
+// });
+// });
